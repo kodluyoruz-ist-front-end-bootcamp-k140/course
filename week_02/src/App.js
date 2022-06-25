cc20eaf0796c467339764a1ba6fb33dcae5e91a9
ugur.erdal <ugur.erdal@cimri.com>
2022-06-25 10:30:24 +0300

    ilk değişiklik

--- week_02/src/App.js
+++ week_02/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React Öğrenin !
         </a>
       </header>
     </div>
